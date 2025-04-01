062be4f76b77fb07b6a51f950697913802121e1b
PeanutNotCashew <ellafan227@gmail.com>
2025-04-01 15:16:43 -0400

    Edits to ufw script

--- linux/firewalls/ubuntu_generic.sh
+++ linux/firewalls/ubuntu_generic.sh
@@ -1,14 +1,25 @@
 #! bin/bash
 
-if ! systemctl list-unit-files --type=service | grep -q "ufw.service"
-then # Install if not already installed
+# usage: ./ubuntu_ufw.sh <PORTS/SERVICES>
+# use if nftables or other script doesn't work ig
+
+if ! systemctl list-unit-files --type=service | grep -q "ufw.service"; then
+    # Install if not already installed
     sudo apt install ufw
-else # Reset if already installed
+else
+    # Reset for a clean slate. Erases rules + turns off ufw
     sudo ufw reset
 fi
+
 # Turn UFW on
 sudo ufw enable
 
+# Deny all connections by default
 sudo ufw default deny
 
 # Rules go under here
+# Rules can be port numbers or services (22, ssh, etc), i think services should be lowercase
+rules=("$@")
+for rule in "${rules[@]}"; do
+    echo "sudo ufw allow $rule"
+done
